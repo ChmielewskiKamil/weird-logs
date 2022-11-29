29d58a6fda440dd6c82425d6c731fe7ac52ce208
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-29 12:45:12 +0100

    chore: copy example from echidna-weird-logs

--- src/WeirdLogs.sol
+++ src/WeirdLogs.sol
@@ -1,5 +1,25 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.6;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract WeirdLogs {
+    bytes32 originalHash = keccak256(abi.encodePacked(address(this)));
+
+    event OriginalHash(bytes32 originalHash);
+    event HashCastedToBytes16(bytes16 originalHash);
+    event HashCastedToBytes8(bytes8 originalHash);
+    event HashCastedToBytes4(bytes4 originalHash);
+    event HashCastedToBytes3(bytes3 originalHash);
+    event HashCastedToBytes2(bytes2 originalHash);
+    event HashCastedToBytes1(bytes1 orignalHash);
+
+    function test_always_false() public {
+        emit OriginalHash(originalHash);
+        emit HashCastedToBytes16(bytes16(originalHash));
+        emit HashCastedToBytes8(bytes8(originalHash));
+        emit HashCastedToBytes4(bytes4(originalHash));
+        emit HashCastedToBytes3(bytes3(originalHash));
+        emit HashCastedToBytes2(bytes2(originalHash));
+        emit HashCastedToBytes1(bytes1(originalHash));
+        assert(false);
+    }
 }
--- src/WeirdLogs.t.sol
+++ src/WeirdLogs.t.sol
@@ -6,17 +6,28 @@ import "ds-test/test.sol";
 import "./WeirdLogs.sol";
 
 contract WeirdLogsTest is DSTest {
-    WeirdLogs logs;
+    // This is the default Echidna contractAddr
+    // see: https://github.com/crytic/echidna/wiki/Config
+    address weirdLogs = address(0x00a329c0648769A73afAc7F9381E08FB43dBEA72);
 
-    function setUp() public {
-        logs = new WeirdLogs();
-    }
+    bytes32 originalHash = keccak256(abi.encodePacked(weirdLogs));
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
+    event OriginalHash(bytes32 originalHash);
+    event HashCastedToBytes16(bytes16 originalHash);
+    event HashCastedToBytes8(bytes8 originalHash);
+    event HashCastedToBytes4(bytes4 originalHash);
+    event HashCastedToBytes3(bytes3 originalHash);
+    event HashCastedToBytes2(bytes2 originalHash);
+    event HashCastedToBytes1(bytes1 orignalHash);
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_always_false() public {
+        emit OriginalHash(originalHash);
+        emit HashCastedToBytes16(bytes16(originalHash));
+        emit HashCastedToBytes8(bytes8(originalHash));
+        emit HashCastedToBytes4(bytes4(originalHash));
+        emit HashCastedToBytes3(bytes3(originalHash));
+        emit HashCastedToBytes2(bytes2(originalHash));
+        emit HashCastedToBytes1(bytes1(originalHash));
+        assert(false);
     }
 }
